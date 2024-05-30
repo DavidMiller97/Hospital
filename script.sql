e0709962da0a204ce85a5861ae92064965a9206e
Pamela Moreno <pamelamoreno@MacBook-Air-de-Pamela.local>
2024-05-29 20:18:26 -0600

    Actualizacion consulta, script y receta

--- script.sql
+++ script.sql
@@ -3,18 +3,15 @@ CREATE DATABASE hospital;
 USE hospital;
 
 CREATE TABLE especialidad (
-    idEspecialidad INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(150) NOT NULL,
-
-    CONSTRAINT pk_especialidad
-    PRIMARY KEY(idEspecialidad)
+    idEspecialidad INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(150) NOT NULL
 );
 
 INSERT INTO especialidad (nombre) VALUES ("Pediatria");
 INSERT INTO especialidad (nombre) VALUES ("Medicina General");
 
 CREATE TABLE medico (
-    idMedico INT NOT NULL AUTO_INCREMENT,
+    idMedico INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellidoPa VARCHAR(50) NOT NULL,
     apellidoMa VARCHAR(50) NULL,
@@ -25,8 +22,6 @@ CREATE TABLE medico (
     admin boolean not null,
     idEspecialidad int not null,
 
-    CONSTRAINT pk_medico
-    PRIMARY KEY (idMedico),
     CONSTRAINT fk_especialidad
     FOREIGN KEY (idEspecialidad) REFERENCES especialidad(idEspecialidad)
 
@@ -36,12 +31,10 @@ INSERT INTO medico (nombre, apellidoPa, apellidoMa, telefono, cedula,correo,pass
 					('Juan', 'González', 'López', '+123456789', 'ABC12345','doctor@gmail.com','1234',1,1);
 
 CREATE TABLE Farmacia (
-    idFarmacia INT NOT NULL AUTO_INCREMENT,
+    idFarmacia INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(150) NOT NULL,
-    descripcion VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL
 
-    CONSTRAINT pk_farmacia
-    PRIMARY KEY (idFarmacia)
 );
 
 INSERT INTO Farmacia (nombre, descripcion) VALUES ('Medicamento A', 'Antiinflamatorio para aliviar el dolor.');
@@ -49,15 +42,12 @@ INSERT INTO Farmacia (nombre, descripcion) VALUES ('Vacuna C', 'Inmunización co
 
 
 CREATE TABLE medicamento (
-    idMedicamento INT NOT NULL AUTO_INCREMENT,
+    idMedicamento INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR (255),
     fechaCaducidad DATE NOT NULL,
     precio DECIMAL(10, 2) NOT NULL,
     ingredientes VARCHAR(255) NULL,
-    descripcion VARCHAR(255) NULL,
-
-    CONSTRAINT pk_medicamento
-    PRIMARY KEY(idMedicamento)
+    descripcion VARCHAR(255) NULL
 );
 
 INSERT INTO medicamento (fechaCaducidad, precio, ingredientes, descripcion) VALUES ('2024-06-30', 25.99, 'Ibuprofeno, Paracetamol', 'Medicamento para el alivio del dolor.');
@@ -70,7 +60,7 @@ CREATE TABLE detalleFarmacia (
     idMedicamento INT NOT NULL,
     idFarmacia INT NOT NULL,
     cantidad INT NOT NULL,
-    CONSTRAINT pk_detalleFarmacia
+   
     PRIMARY KEY (idMedicamento, idFarmacia),
     CONSTRAINT fk_detalleFarmacia_medicamento
     FOREIGN KEY (idMedicamento) REFERENCES medicamento(idMedicamento),
@@ -82,27 +72,23 @@ INSERT INTO detalleFarmacia (idMedicamento, idFarmacia, cantidad) VALUES (1, 1, 
 
 
 CREATE TABLE paciente (
-    idPaciente INT NOT NULL AUTO_INCREMENT,
+    idPaciente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellidoPaterno VARCHAR(100) NOT NULL,
     apellidoMaterno VARCHAR(100) NULL,
     fechaNacimiento date not null,
-    correo VARCHAR(100) NOT NULL UNIQUE,
-    CONSTRAINT pk_paciente
-    PRIMARY KEY(idPaciente)
+    correo VARCHAR(100) NOT NULL UNIQUE
 );
 
 INSERT INTO paciente (nombre, apellidoPaterno, apellidoMaterno,fechaNacimiento,correo) VALUES ('Juan', 'Martínez', 'López','25-02-20','juan.lopez@example.com');
 
 CREATE TABLE consulta (
-    idConsulta INT NOT NULL AUTO_INCREMENT,
+    idConsulta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idPaciente INT NOT NULL,
     idMedico INT NOT NULL,
     fecha DATE NOT NULL,
     padecimiento TEXT NULL,
     
-    CONSTRAINT pk_consulta
-    PRIMARY KEY(idConsulta),
     CONSTRAINT fk_consulta_paciente
     FOREIGN KEY (idPaciente) REFERENCES paciente(idPaciente),
     CONSTRAINT fk_consulta_medico
@@ -114,18 +100,14 @@ INSERT INTO consulta (idPaciente,idMedico,fecha,padecimiento) VALUES (1,1,'2025-
 
 
 CREATE TABLE receta (
-    idReceta INT NOT NULL AUTO_INCREMENT,
+    idReceta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idConsulta INT NOT NULL UNIQUE,
     fecha DATE NOT NULL,
     indicaciones TEXT NULL,
     comentarios TEXT NULL,
 
-    CONSTRAINT pk_receta
-    PRIMARY KEY(idReceta),
     CONSTRAINT fk_receta_consulta
-    FOREIGN KEY(idConsulta) REFERENCES consulta(idConsulta),
-    CONSTRAINT uq_consulta
-    UNIQUE(idConsulta)
+    FOREIGN KEY(idConsulta) REFERENCES consulta(idConsulta)
 );
 
 INSERT INTO receta (idConsulta,fecha, indicaciones, comentarios) VALUES (1,'2024-04-10', 'Tomar medicamento A cada 8 horas', 'Ninguno');
@@ -135,7 +117,6 @@ CREATE TABLE detallesReceta (
     idMedicamento INT NOT NULL,
     descripcion VARCHAR(255) NULL,
 
-    CONSTRAINT pk_detallesReceta
     PRIMARY KEY(idReceta, idMedicamento),
     CONSTRAINT fk_detallesReceta_receta
     FOREIGN KEY(idReceta) REFERENCES receta(idReceta),
